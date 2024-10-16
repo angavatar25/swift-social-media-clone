1e0c48bae4315a6f690f82360ab1cf82524f06b1
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-10-16 11:13:28 +0700

    Initial Commit

---
+++ Social Media Clone/Social_Media_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Media_CloneApp.swift
+//  Social Media Clone
+//
+//  Created by Ananda Afryan on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_Media_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
